235aa436eaa3a01c617183dedeee25f117137293
Yevhen Kononenko <areyouready575@gmail.com>
2023-04-20 14:39:01 +0300

    configure project

---
+++ JatApp Test Project/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  JatApp Test Project
+//
+//  Created by Yevhen Kononenko on 20.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
---
+++ JatApp Test ProjectUITests/JatApp_Test_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  JatApp_Test_ProjectUITestsLaunchTests.swift
+//  JatApp Test ProjectUITests
+//
+//  Created by Yevhen Kononenko on 20.04.2023.
+//
+
+import XCTest
+
+final class JatApp_Test_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
